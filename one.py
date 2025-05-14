fbd4b607cf6effa194e9bb3b5a6f4da1b4343c32
SHAMIM AKHTAR <shamim2707@gmail.com>
2025-05-14 17:26:31 +0530

    Variables and print

--- one.py
+++ one.py
@@ -1,5 +1,38 @@
 print("Hello, World!")
 print("Shamim Akhtar")
-print("Python is a programming language.")
-print("I am learning Python.")
 
+
+print(23)
+print(23.5+23.5)
+
+
+# Integer
+print(42)
+
+# Float
+print(3.14)
+
+# String
+print("This is a string")
+
+# Boolean
+print(True)
+print(False)
+
+# List
+print([1, 2, 3, 4, 5])
+
+# Tuple
+print((1, 2, 3, 4, 5))
+
+# Set
+print({1, 2, 3, 4, 5})
+
+# Dictionary
+print({"key1": "value1", "key2": "value2"})
+
+# NoneType
+print(None)
+
+# Complex number
+print(3 + 4j)
---
+++ two.py
@@ -0,0 +1,47 @@
+name = "Shamim Akhtar"
+age = 30
+print("Name:", name, type(name))
+print("Age:", age, type(age))
+# This code demonstrates the use of different data types in Python.
+
+
+
+
+
+
+
+# Integer
+integer_var = 10
+print("Integer:", integer_var, type(integer_var))
+
+# Float
+float_var = 10.5
+print("Float:", float_var, type(float_var))
+
+# String
+string_var = "Hello, World!"
+print("String:", string_var, type(string_var))
+
+# Boolean
+boolean_var = True
+print("Boolean:", boolean_var, type(boolean_var))
+
+# List
+list_var = [1, 2, 3, 4, 5]
+print("List:", list_var, type(list_var))
+
+# Tuple
+tuple_var = (1, 2, 3, 4, 5)
+print("Tuple:", tuple_var, type(tuple_var))
+
+# Set
+set_var = {1, 2, 3, 4, 5}
+print("Set:", set_var, type(set_var))
+
+# Dictionary
+dict_var = {"key1": "value1", "key2": "value2"}
+print("Dictionary:", dict_var, type(dict_var))
+
+# NoneType
+none_var = None
+print("NoneType:", none_var, type(none_var))
